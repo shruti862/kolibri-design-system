3c816cf866c61be0d1fc37ee757a8add8c739809
Richard Tibbles <richard@learningequality.org>
2024-12-10 09:01:08 -0800

    Cleanup linting config for kolibri-format. Tweak for v-deep.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,1 +1,5 @@
-module.exports = require('kolibri-tools/.stylelintrc');
+const stylelintConfig = require('kolibri-format/.stylelintrc');
+
+stylelintConfig['rules']['selector-pseudo-element-no-unknown'] = [true, { ignorePseudoElements: ['v-deep'] }];
+
+module.exports = stylelintConfig;
